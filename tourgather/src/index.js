67c30cbeb1162d61c4540274a274984c9be5e158
hua-lun <chuahualun@gmail.com>
2022-06-04 21:58:39 +0800

    Add login and signup files

--- tourgather/src/index.js
+++ tourgather/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from "./App"
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 import './index.css';
 
@@ -9,7 +11,6 @@ import './index.css';
 
 
 import { BrowserRouter } from "react-router-dom";
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 // const store = configureStore(rootReducer)
